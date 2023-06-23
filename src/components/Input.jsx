a619fcd293ce539ab740d5230687f26ee875606b
podoDJ <ourstar1345@gmail.com>
2023-06-23 16:45:24 +0900

    save button alert added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,22 +4,34 @@ import { StButton } from './Button'
 
 const Input = () => {
 
-  const [inputNumber, setInputNumber] = useState("")
+  const [inputName, setInputName] = useState("")
+  const [inputPrice, setInputPrice] = useState("")
+
   //toLocaleString()이 콤마를 표현하는 메인 메서드.
-  const commaNumber = (event) => {
-    const inputValue = event.target.value
-    const removedCommaValue = Number(inputValue.replaceAll(",",""))
-    console.log(removedCommaValue)
-    setInputNumber(removedCommaValue.toLocaleString())
+  const nameInputHandler = (event) => {
+    const newInputName = event.target.value
+    setInputName(newInputName)
+  }
+
+  const priceInputHandler = (event) => {
+    const newInputPrice = event.target.value
+    const removedCommaNewInputPrice = Number(newInputPrice.replaceAll(",",""))
+    setInputPrice(removedCommaNewInputPrice.toLocaleString())
+  }
+
+  const alertInput = (event) => {
+    event.preventDefault();
+    console.log("inputName, inputPrice => ", inputName, inputPrice)
+    alert(`이름: ${inputName}  가격: ${inputPrice}`)
   }
   return (
     <div>
       <h1>Input</h1>
-      <form>
+      <form onSubmit={alertInput}>
         <StInputCtn>
-          <div>이름<StNameInput/></div>
+          <div>이름<StNameInput value={inputName} onChange={nameInputHandler}/></div>
           {/* 가격의 input에 type이 꼭 text여야 한다. 왜냐하면 ,는 type이 number일 때 표현이 안 되어 에러가 뜨기 때문이다. */}
-          <div>가격<StNameInput type="text" value={inputNumber} onChange={commaNumber}/></div>
+          <div>가격<StNameInput type="text" value={inputPrice} onChange={priceInputHandler}/></div>
           <StButton borderColor="rgb(85, 239, 196)" backgroundColor="rgb(85, 239, 196)" height="40px" width="100px">저장</StButton>
         </StInputCtn>
 
